0fe2d452ebe4bede6a10b7826e84c5ec90e18bad
JuAnDaGmiss77 <juandavidb741@gmail.com>
2024-03-29 22:41:35 -0400

    add model to translation

--- translation.js
+++ translation.js
@@ -1,4 +1,19 @@
-import { HfInference } from "@huggingface/inference"
-import { config } from "dotenv"
+import { HfInference } from "@huggingface/inference";
+import { config } from "dotenv";
 
-config()
+config();
+
+const hf = new HfInference(process.env.HF_ACCESS_TOKEN);
+const model = "facebook/mbart-large-50-many-to-many-mmt";
+const textToTranslate = 'Hi my name is David'
+
+const response = await hf.translation({
+  model: model,
+  inputs: textToTranslate,
+  parameters: {
+    src_lang: "en_XX",
+    tgt_lang: "es_XX",
+  },
+});
+
+console.log(response)
